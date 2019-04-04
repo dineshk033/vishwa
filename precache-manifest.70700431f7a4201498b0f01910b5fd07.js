5e1f0208e5015e64f46d931e0e767f742f5fd82e
dineshk033 <dineshk033@gmail.com>
2019-04-04 20:07:01 +0530

    Updates

---
+++ precache-manifest.70700431f7a4201498b0f01910b5fd07.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "7a16bb79f5d34fec26cb6a340db35def",
+    "url": "/vishwa/static/media/refurbish_banner.7a16bb79.jpg"
+  },
+  {
+    "revision": "ced2ab713c365089213db5026b162bee",
+    "url": "/vishwa/static/media/maxresdefault.ced2ab71.jpg"
+  },
+  {
+    "revision": "265de63d8cb8b656493db89a2164de9c",
+    "url": "/vishwa/static/media/Benefits.265de63d.jpg"
+  },
+  {
+    "revision": "25d2f7a4b93012d5852ac4778c9c4c66",
+    "url": "/vishwa/static/media/01.25d2f7a4.jpg"
+  },
+  {
+    "revision": "047080fe3453901b5f01",
+    "url": "/vishwa/static/js/runtime~main.350a7999.js"
+  },
+  {
+    "revision": "35dae05229940069f443",
+    "url": "/vishwa/static/js/main.cd191a4c.chunk.js"
+  },
+  {
+    "revision": "b1b9f719c0a85db3b49a",
+    "url": "/vishwa/static/js/3.2e0c05c9.chunk.js"
+  },
+  {
+    "revision": "b3d9f6b0ebddcbb28cbb",
+    "url": "/vishwa/static/js/2.cfe7a0a5.chunk.js"
+  },
+  {
+    "revision": "35dae05229940069f443",
+    "url": "/vishwa/static/css/main.558b0f01.chunk.css"
+  },
+  {
+    "revision": "14142a5b0df68c9936ef1e6f3e922dc6",
+    "url": "/vishwa/index.html"
+  }
+];
